b5b1dad706c0cec206109e8d06f380c5cd0cd337
Jati Pikukuh <jati.pikukuh@maiil.ugm.ac.id>
2019-03-12 09:29:36 +0700

    my pwa

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     // inisiasi API url
     var _url = 'https://my-json-server.typicode.com/techtona/belajar-api/mahasiswa';
+    // var _url = 'http://localhost/b2/index.php';
 
     // untuk menampung data semua mahasiswa
     var result = '';
@@ -11,23 +12,92 @@ $(document).ready(function () {
     var gender = [];
 
 
-    $.get(_url,function (data) {
+    // $.get(_url,function (data) {
+    function renderPage(data) {
         $.each(data, function (key, items) {
             _gend = items.gender;
 
             result += '<div>' +
-                '<h3>'+items.name+'</h3>' +
-                '<p>'+_gend+'</p>' +
+                '<h3>' + items.name + '</h3>' +
+                '<p>' + _gend + '</p>' +
                 '</div>';
 
-            if ($.inArray(_gend, gender) === -1){
+            if ($.inArray(_gend, gender) === -1) {
                 gender.push(_gend);
-                gender_result += "<option value='"+_gend+"'>"+_gend+"</option>";
+                gender_result += "<option value='" + _gend + "'>" + _gend + "</option>";
             }
-
         });
 
         $('#mhs-list').html(result);
-        $('#mhs-select').html(gender_result);
+        $('#mhs-select').html("<option value='semua'>semua</option>" + gender_result);
+        // });
+    }
+
+    var networkDataReceive = false;
+    /*
+    * start balapan antara service dengan cache
+    * fresh data from online service
+    * */
+    var networkUpdate = fetch(_url).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        networkDataReceive = true;
+        renderPage(data);
     });
+
+    /* ambilkan data dalam local cache */
+    caches.match(_url).then(function (response) {
+        if (!response) throw Error("no data on cache")
+        return response.json();
+    }).then(function (data) {
+        if (!networkDataReceive){
+            renderPage(data);
+            console.log("render from cache");
+        }
+    }).catch(function () {
+        return networkUpdate;
+    });
+
+    // filter data
+
+    $("#mhs-select").on('change', function () {
+        updateListMahasiswa($(this).val());
+    });
+
+    function updateListMahasiswa(opt) {
+        var result = '';
+        var _url2 = _url;
+
+        // cek opsi yang dipilih
+        if (opt !== 'semua'){
+            _url2 = _url + '?gender='+opt;
+        }
+
+        $.get(_url2,function (data) {
+            $.each(data, function (key, items) {
+                _gend = items.gender;
+
+                result += '<div>' +
+                    '<h3>'+items.name+'</h3>' +
+                    '<p>'+_gend+'</p>' +
+                    '</div>';
+            });
+
+            $('#mhs-list').html(result);
+        });
+    }
 });
+
+if ('serviceWorker' in navigator){
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/serviceworker.js').then(
+            function (reg) {
+                // registerasi service worker berhasil
+                console.log('SW registration success, scope :',reg.scope);
+            }, function (err) {
+                // reg failed
+                console.log('SW registration faild : ', err);
+            }
+        )
+    })
+}
---
+++ serviceworker.js
@@ -0,0 +1,83 @@
+var CACHE_NAME = 'latihan-pwa-cache-v1';
+
+var urlsToCache = [
+    '/',
+    '/js/main.js',
+    '/js/jquery.min.js',
+    '/css/main.css',
+    '/images/ugm.png'
+];
+
+// install cache on browser
+self.addEventListener('install', function (event) {
+    // do install
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                // cek apakah cache sudah terinstall
+                console.log("service worker do install..");
+                return cache.addAll(urlsToCache);
+            }
+        )
+    )
+});
+
+// aktivasi sw
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                // jika sudah ada cache dgn versi beda maka di hapus
+                cacheNames.filter(function (cacheName) {
+                    return cacheName !== CACHE_NAME;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+// fetch cache
+self.addEventListener('fetch', function (event) {
+    var request = event.request;
+    var url = new URL(request.url);
+
+    /*
+    * menggunakan data local cache
+    * */
+    if (url.origin === location.origin){
+        event.respondWith(
+            caches.match(request).then(function (response) {
+                // jika ada data di cache maka tampilkan data cache, jika tidak maka petch request
+                return response || fetch(request);
+            })
+        )
+    } else{
+        // internet API
+        event.respondWith(
+            caches.open('mahasiswa-cache-v1').then(function (cache) {
+                return fetch(request).then(function (liveRequest) {
+                    cache.put(request, liveRequest.clone());
+                    // save cache to mahasiswa-cache-v1
+                    return liveRequest;
+                }).catch(function () {
+                    return caches.match(request).then(function (response) {
+                        if (response) return response;
+                        return caches.match('/fallback.json');
+                    })
+                })
+            })
+        )
+    }
+
+    // event.respondWith(
+    //     caches.match(event.request).then(function (response) {
+    //         console.log(response);
+    //         if (response){
+    //             return response;
+    //         }
+    //         return fetch(event.request);
+    //     })
+    // )
+});
